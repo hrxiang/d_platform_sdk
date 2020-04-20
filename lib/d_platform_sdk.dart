b4615c73186692ad634a6fc514b59be7b32a1b5f
xianghairui <xianghairui@up-tech.cn>
2020-04-20 15:34:30 +0800

    添加 downloadUrl

--- lib/d_platform_sdk.dart
+++ lib/d_platform_sdk.dart
@@ -26,13 +26,13 @@ class DPlatformSdk {
     String downloadUrl,
   }) {
     // 区分当前事件类型
-    params.putIfAbsent("action", () => action);
+    params?.putIfAbsent("action", () => action);
     // 被唤起的应用的scheme
-    params.putIfAbsent("urlString", () => urlString);
+    params?.putIfAbsent("urlString", () => urlString);
     // 被唤起的应用的包名
-    params.putIfAbsent("packageName", () => packageName);
+    params?.putIfAbsent("packageName", () => packageName);
     // 被唤起的应用的下载地址
-    params.putIfAbsent("downloadUrl", () => downloadUrl);
+    params?.putIfAbsent("downloadUrl", () => downloadUrl);
     // 启动scheme
     _channel.invokeMethod("call", params);
   }
