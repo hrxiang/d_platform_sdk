6107121353b7032d0396d0b72e1340a982e5621a
xianghairui <xianghairui@up-tech.cn>
2020-05-11 17:22:25 +0800

    fixed bug

--- ios/Classes/DPlatformSdkPlugin.m
+++ ios/Classes/DPlatformSdkPlugin.m
@@ -1,5 +1,5 @@
 #import "DPlatformSdkPlugin.h"
-#import <flutter_platform_sdk/flutter_platform_sdk-Swift.h>
+#import <d_platform_sdk/flutter_platform_sdk-Swift.h>
 
 @implementation DPlatformSdkPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
