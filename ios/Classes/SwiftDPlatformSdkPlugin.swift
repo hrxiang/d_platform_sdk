a3398b19f024cedbb2880dd5221f22395a9b8b29
tangyingli <tangyingli@kofo.io>
2020-06-11 16:42:36 +0800

    接入ios

--- ios/Classes/SwiftDPlatformSdkPlugin.swift
+++ ios/Classes/SwiftDPlatformSdkPlugin.swift
@@ -2,13 +2,32 @@ import Flutter
 import UIKit
 
 public class SwiftDPlatformSdkPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "d_platform_sdk", binaryMessenger: registrar.messenger())
-    let instance = SwiftDPlatformSdkPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "d_platform_sdk", binaryMessenger: registrar.messenger())
+        let instance = SwiftDPlatformSdkPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard
+            "call" == call.method,
+            var args = call.arguments as? [String : Any]
+        else {
+            print("Invalid flutter call.")
+            return
+        }
+        guard
+            let action = args["action"] as? String,
+            let token = args["token"] as? String,
+            let orderSn = args["orderSn"] as? String,
+            let channelNo = args["channelNo"] as? String
+        else {
+            print("The required parameters are missing.")
+            return
+        }
+        
+        if "pay" == action {
+            
+        }
+    }
 }
