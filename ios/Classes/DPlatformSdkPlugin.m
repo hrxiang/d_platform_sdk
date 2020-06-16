39b599ef430010a579d0d02cca45e492f5fcfb1e
tangyingli <tangyingli@kofo.io>
2020-06-16 16:03:19 +0800

    ios

--- ios/Classes/DPlatformSdkPlugin.m
+++ ios/Classes/DPlatformSdkPlugin.m
@@ -1,5 +1,5 @@
 #import "DPlatformSdkPlugin.h"
-#import <flutter_dplatform_sdk/flutter_platform_sdk-Swift.h>
+#import <flutter_dplatform_sdk/flutter_dplatform_sdk-Swift.h>
 
 @implementation DPlatformSdkPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
